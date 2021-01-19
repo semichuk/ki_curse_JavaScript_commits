ed19a46e293e36d8eeb244a462e9c2886c75b77c
ki <semichuk15@gmail.ru>
2021-01-19 17:11:58 +0400

    commit №5

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-"use strict";
-let x = 'game';
+let numberOfFilms = +prompt("How many films have you seen?",0);
+console.log(numberOfFilms);
 
-console.log(`https://www.ki.ru/${x}/kirill/`);
-let userki = 5 ;
-alert(`Hello,${userki},I am waiting you   `);
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false
+};
+
+let a = prompt("Last movie, which you have seen:",""),
+    b = prompt('How was it  ?', ''),
+    c = prompt("Last movie, which you have seen:",""),
+    d = prompt('How was it  ?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+ 
+console.log(personalMovieDB.movies);
+console.log(personalMovieDB);
