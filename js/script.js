15efdc1fc5f76180634fe96d18c7cd43d9fcd963
ki <semichuk15@gmail.ru>
2021-01-27 17:39:15 +0400

    commit №17

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 "use strict";
+
+let num = 15;
+
+function saySomeThing(text)
+{
+    console.log(text);
+    num = 10;
+}
+
+function plus(a,b)
+{
+    return (a + b);
+}
+
+function reti()
+{
+    let num = 20;
+    return num;
+}
+
+const xreti = reti();
+
+const x = function()
+{
+    console.log('hello expression');
+};
+
+const y = (a,g) => {a + g};
+
+const stri = 'testy';
+
+console.log(stri.toUpperCase());
+console.log(stri);
--- js/script_practica.js
+++ js/script_practica.js
@@ -1,21 +1,47 @@
 "use strict";
+
+let numberOfFilms = 0;
+
 const  DB = 
 {
+    count: numberOfFilms,
     movies:[]
 };
 
-for (let i = 0; i < 2; i++) {
-    
+let i = 0;
+
+while(++i < 2) 
+{
     const a = prompt('which?',''),
         b = prompt('how is it?','');
     if (a != null && b != null && a != '' && b != '' && a.length < 50) 
     {
         DB.movies[a] = b;
-        alert('done');
+        alert(i);
     }else 
     {
         alert('Error etner');
         i--;
     }
+    alert(a);
+    
+}
+
+numberOfFilms = prompt('how many?','');
+
+alert(typeof(numberOfFilms));
+
+if(numberOfFilms < 10)
+{
+    alert('you have few watched films');
+}else if(numberOfFilms <= 30 && numberOfFilms >= 10 )
+{
+    alert('you have watched medium');
+}else if(numberOfFilms > 30)
+{
+    alert('you are cool guy');
+}else
+{
+    alert('error 304');
 }
 console.log(DB);
