ad1e589316dbb3f73b250aea60cffb4e25fc7957
ki <semichuk15@gmail.ru>
2021-02-10 11:25:54 +0400

    commit №233

--- js/script_practica.js
+++ js/script_practica.js
@@ -1,113 +1,93 @@
 "use strict";
 
-let numberOfFilms;
-
-
-
-function start()
+let personalMovieDB = 
 {
-    do
-    {
-        numberOfFilms = +prompt('how many?','');
-    }while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms));
-
-}
-
-
-
-
-
-
-
-const  DB = 
-{
-    count: numberOfFilms,
+    count: 0,
     movies:[],
     actors:{},
     genres:[],
-    privat: ''
-};
-
-
-
-
-
-
-
-function rememberMyFilms()
-{
-    let i = 0;
-
-while(++i < 2) 
-{
-    const a = prompt('which?',''),
-        b = prompt('how is it?','');
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) 
+    privat: '',
+    start: function ()
     {
-        DB.movies[a] = b;
-        alert(i);
-    }else 
+        do
+        {
+            personalMovieDB.count = +prompt('how many?','');
+        }while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count));
+    
+    },
+
+    
+    
+    rememberMyFilms: function ()
     {
-        alert('Error etner');
-        i--;
+        let i = 0;
+
+        while(++i < 2) 
+    {
+        const a = prompt('which?',''),
+            b = prompt('how is it?','');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) 
+        {
+            DB.movies[a] = b;
+            alert(i);
+        }else 
+        {
+            alert('Error etner');
+            i--;
+        }
+        alert(a);
+    
     }
-    alert(a);
+    },
+
+
+
+
+   
+   
+    detectPersonalLevel: function (){
+
+        if(numberOfFilms < 10)
+        {
+            alert('you have few watched films');
+        }else if(numberOfFilms <= 30 && numberOfFilms >= 10 )
+        {
+            alert('you have watched medium');
+        }else if(numberOfFilms > 30)
+        {
+            alert('you are cool guy');
+        }else
+        {
+            alert('error 304');
+        }
+    },
+        
+    
+    
+    showMyDB: function (privat)
+    {
+        if(!privat)
+        {
+            console.log(DB);
+        }else
+        {
+            console.log('privat is true');
+        }
+    },
+        
+        
+    writeYourGenres: function ()
+    {
+        for (let i = 1;i <= 3; i++) 
+        {
+            DB.genres[i - 1] = prompt(`Your the most love genre №${i}`);
+        }
+    }
     
 }
-}
 
 
 
 
 
-
-
-
-
-
-
-function detectPersonalLevel(){
-
-if(numberOfFilms < 10)
-{
-    alert('you have few watched films');
-}else if(numberOfFilms <= 30 && numberOfFilms >= 10 )
-{
-    alert('you have watched medium');
-}else if(numberOfFilms > 30)
-{
-    alert('you are cool guy');
-}else
-{
-    alert('error 304');
-}
-}
-
-
-
-
-
-
-
-
-
-function showMyDB(privat)
-{
-    if(!privat)
-    {
-        console.log(DB);
-    }else
-    {
-        console.log('privat is true');
-    }
-}
-
-function writeYourGenres()
-{
-    for (let i = 1;i <= 3; i++) 
-    {
-        DB.genres[i - 1] = prompt(`Your the most love genre №${i}`);
-    }
-}
-
-writeYourGenres();
