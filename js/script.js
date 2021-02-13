fe01ffcce13cd31fa65afa90a68f6c3ca978ab4c
ki <semichuk15@gmail.ru>
2021-02-14 01:12:06 +0400

    commit №28

--- js/script.js
+++ js/script.js
@@ -1,41 +1,5 @@
-// 'use strict';
-
-// // const arr = [2,3,4,5,6,7],
-// //     arr2 = [...arr] ;
-
-
-
-// //     arr2[5] = 6666666;
-
-// // console.log(arr2,arr);
-
-// function logg(a,b,c=0)
-// {
-// console.log(a);
-// console.log(b);
-// console.log(c);
-
-// }
-
-
-
-// const num = [4,6,7,9];
-
-// logg(...num);
-
-const arr = [3,4];
-
-const narr = [...arr];
-
-const h = {
-    one: 1,
-    two: 2
-}
-
-const newh = {
-    ...h,
-    
-
-};
-
-console.log(newh);
+ 'use strict';
+const button = document.getElementsByClassName('button');
+const button2 = document.querySelectorAll('#box2.button.large2')
+const button3 = document.querySelector('#box.button.large');
+console.log(button3);
