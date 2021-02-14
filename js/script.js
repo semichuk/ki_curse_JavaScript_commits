a0d6f2509461ee3f5347226eca49e3285e73d169
ki <semichuk15@gmail.ru>
2021-02-14 17:02:19 +0400

    commit №29

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
  'use strict';
 const button = document.getElementsByClassName('button');
-const button2 = document.querySelectorAll('#box2.button.large2')
-const button3 = document.querySelector('#box.button.large');
-console.log(button3);
+const button2 = document.querySelector('#box2');
+button2.style.backgroundColor = 'blue';
+button[0].style.cssText = 'background-color : red;'
